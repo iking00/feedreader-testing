38b00caa926d8e9b30d26a928caca239d3ce8872
Ian King <ian_king@comcast.net>
2017-12-10 16:31:11 -0500

    docs: updating comments

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -9,28 +9,15 @@
  * to ensure they don't run until the DOM is ready.
  */
 $(function() {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
+    //Test the allFeeds variable is defined appropriately
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
+        //test that allFeeds is defined and not empty
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
 
-
-        /* Test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
+         //test that each index of allFeeds has a url property that is not empty
          it('have a url', function() {
             allFeeds.forEach(function(feed) {
                 const url = feed.url;
@@ -40,11 +27,7 @@ $(function() {
             });
          });
 
-
-        /* Test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
+         //test that each index of allFeeds has a name property that is not empty
          it('have a name', function() {
             allFeeds.forEach(function(feed) {
                 const name = feed.name;
@@ -55,82 +38,76 @@ $(function() {
          });
     });
 
-
-    /* TODO: Write a new test suite named "The menu" */
-
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
-
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+    //test the visibility and toggle of the menu
     describe('The menu', function() {
         const bodyElement = $('body')[0];
         const menuIcon = $('.menu-icon-link')[0];
         let thisClass;
 
+        //menu should initially be hidden by assigning body the appropriate class
         it('is hidden', function() {
             thisClass = bodyElement.className;
             expect(thisClass).toContain('menu-hidden');
         });
 
+        //menu should toggle when menu is clicked
         it('toggles when menu clicked', function() {
+            //trigger menu click
             menuIcon.click();
+            //class should be toggled on body
             thisClass = bodyElement.className;
             expect(thisClass).not.toContain('menu-hidden');
+            //trigger menu click
             menuIcon.click();
+            //class should be toggled on body
             thisClass = bodyElement.className;
             expect(thisClass).toContain('menu-hidden');
         });
     });
-    /* TODO: Write a new test suite named "Initial Entries" */
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+    //test loadFeed appends .entry elements to container when complete
     describe('Initial Entries', function() {
+        //loadFeed will get data and append to container
+        //include done to account for async
         beforeEach(function(done) {
-            loadFeed(0,function() {
+            loadFeed(0, function() {
                 done();
             });
         });
+
+        //when done there should be more than 0 elements with .entry in .feed container
         it('are grabbed and displayed', function(done) {
             expect($('.feed .entry').length).not.toEqual(0);
             done();
         });
     });
-    /* TODO: Write a new test suite named "New Feed Selection" */
 
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
+    //test that loading a new feed changes content
     describe('New Feed Selection', function() {
         let feedTitle, 
             firstEntry;
         let headerTitle = $('.header-title');
 
+        //loadFeed with a index from allFeeds
+        //include done to account for async
         beforeEach(function(done) {
             loadFeed(1, function() {
+                //store the name and first .entry content from first feed
                 feedTitle = headerTitle.html();
                 firstEntry = $('.feed .entry:first h2').html();
+                //call back should trigger a second call to load feed with a different index to mimic a change
                 loadFeed(0, function() {
                     done();
                 })
             });
         });
 
+        //when done the content from second feed should be different than what was loaded from first
         it('changes content', function(done) {
+            //store the name and first .entry content from the second feed
             const newFeedTitle = headerTitle.html();
             const newFirstEntry = $('.feed .entry:first h2').html();
+            //the title and first .entry element should be different between feeds
             expect(newFeedTitle).not.toBe(feedTitle);
             expect(newFirstEntry).not.toBe(firstEntry);
             done();
